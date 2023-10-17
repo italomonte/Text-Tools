bfedb469a6a86ece380d14f67a38667df8488896
Italo Monte <italopessoal@outlook.com>
2023-10-17 00:29:53 -0300

    Updated all the layout of the site

---
+++ text_tools/static/text_tools/js/main.js
@@ -0,0 +1,24 @@
+function charactersCounter(){
+    var text = document.getElementById("text-input").value;
+
+    var numberCaracter = text.length;
+
+    document.getElementById("numeric-results").innerHTML = "Number of Characters = " + numberCaracter;
+}
+
+function upper(){
+    var text = document.getElementById("text-input").value;
+
+    var newtext = text.toUpperCase();
+
+    document.getElementById("text-results").innerHTML = newtext;
+}
+
+
+function lower(){
+    var text = document.getElementById("text-input").value;
+
+    var newtext = text.toLowerCase();
+
+    document.getElementById("text-results").innerHTML = newtext;
+}
