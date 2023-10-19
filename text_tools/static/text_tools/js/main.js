29673ecdbe9a7fa8ed2221513d48294abff6fc9d
Italo Monte <italopessoal@outlook.com>
2023-10-19 02:07:32 -0300

    (maybe) the final comit
    in this commit I fix some design errors, such as the width of the buttons and the color of the box text

--- text_tools/static/text_tools/js/main.js
+++ text_tools/static/text_tools/js/main.js
@@ -23,11 +23,28 @@ function lower(){
     document.getElementById("text-results").innerHTML = newtext;
 }
 
-function abc(){
+
+function reverserString(){
     var text = document.getElementById("text-input").value;
 
-    var newtext = text.toUpperCase();
+    var newtext = text.split('').reverse().join('');
 
     document.getElementById("text-results").innerHTML = newtext;
 }
 
+
+function capitalize(){
+
+    var text = document.getElementById("text-input").value;
+
+    
+    if (text.length === 0) {
+        document.getElementById("text-results").innerHTML = text;
+    }
+
+    var newtext = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+
+
+    document.getElementById("text-results").innerHTML = newtext;
+    
+}
