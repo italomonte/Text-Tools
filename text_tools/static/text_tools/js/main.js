a5dbb0595b255ecdc43010cb673428c8df964e77
Italo Monte <italopessoal@outlook.com>
2023-10-19 01:18:43 -0300

    new functions

--- text_tools/static/text_tools/js/main.js
+++ text_tools/static/text_tools/js/main.js
@@ -22,3 +22,12 @@ function lower(){
 
     document.getElementById("text-results").innerHTML = newtext;
 }
+
+function abc(){
+    var text = document.getElementById("text-input").value;
+
+    var newtext = text.toUpperCase();
+
+    document.getElementById("text-results").innerHTML = newtext;
+}
+
